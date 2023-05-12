898d302ded0f99a9f5883d011a63bc5e7902feaf
danxxo <nikitka.sarkisyan@mail.ru>
2023-05-13 01:47:52 +0300

    ebbaaaaaat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Импортируем необходимые библиотеки
+from typing import List
 import networkx as nx
-import tkinter as tk
+import requests
 
 # Определяем класс для нелюдей
 class Person:
@@ -14,21 +15,11 @@ class Person:
 class SixDegreesApp:
     def __init__(self):
         self.graph = nx.Graph()
-        self.root = tk.Tk()
+        self.exceptions = 0
+        self.connections = {}
+        self.token = '08f8682608f8682608f86826850bec68c4008f808f868266ca567b35dc8f37858cb4172'
+        self.version = 5.92
 
-        # Создаем элементы интерфейса
-        self.label1 = tk.Label(self.root, text="Введите имя первого человека:")
-        self.label1.pack()
-        self.entry1 = tk.Entry(self.root)
-        self.entry1.pack()
-
-        self.label2 = tk.Label(self.root, text="Введите имя второго человека:")
-        self.label2.pack()
-        self.entry2 = tk.Entry(self.root)
-        self.entry2.pack()
-
-        self.button = tk.Button(self.root, text="Найти кратчайший путь", command=self.find_shortest_path)
-        self.button.pack()
 
     # Метод для добавления человека в граф
     def add_person(self, name):
@@ -49,19 +40,86 @@ class SixDegreesApp:
             if node.name == name:
                 return node
         return None
+    
+    def update_amount(self, id1, ids : List):
+        if ids != 'private':
+            self.add_person(id1)
+            for id2 in ids:
+                self.add_person(id2)
+                self.add_connection(id1, id2)
+                self.connections[f'{id1}<->{id2}'] = 1
+            
+
+
+    # def find_shortest_path(self, name1, name2):
+    #     person1 = self.get_person(name1)
+    #     person2 = self.get_person(name2)
+    #     shortest_path = nx.shortest_path(self.graph, person1, person2)
+    #     print(f"Кратчайший путь между {name1} и {name2}: {[node.name for node in shortest_path]}")
+    #     print(nx.shortest_path_length(self.graph, person1, person2))
 
     # Метод для поиска кратчайшего пути между двумя людьми в графе
-    def find_shortest_path(self):
-        name1 = self.entry1.get()
-        name2 = self.entry2.get()
+    def find_shortest_path(self, name1, name2):
         person1 = self.get_person(name1)
         person2 = self.get_person(name2)
-        shortest_path = nx.shortest_path(self.graph, person1, person2)
-        print(f"Кратчайший путь между {name1} и {name2}: {[node.name for node in shortest_path]}")
+
+        try:
+            distance = nx.shortest_path_length(self.graph, person1, person2)
+            if distance != 0:
+                self.connections[f'{name1}<->{name2}'] = distance
+
+        except nx.exception.NetworkXNoPath:
+            self.exceptions += 1
+
+        
 
     # Метод для запуска приложения
-    def run(self):
-        self.root.mainloop()
+    def run(self, name1, name2):
+        self.find_shortest_path(name1, name2)
+
+    def create_response(self, first_id):
+        return requests.get('https://api.vk.com/method/friends.get',
+                                params={
+                                    'access_token' : self.token,
+                                    'v' : self.version,
+                                    'user_id' : first_id
+                                })
+    
+    def get_data_from_response(self, first_id):
+        response = self.create_response(first_id)
+        print(response)
+        try:
+            data = response.json()['response']['items']
+            return data
+        except:
+            return 'private'
+
+
+    # from vk
+    def popopo(self, first_id):
+
+        data = self.get_data_from_response(first_id)
+
+        self.update_amount(first_id, data)
+
+        id2 = data[2]
+        id3 = data[3]
+        data = self.get_data_from_response(id2)
+        self.update_amount(id2, data)
+
+        data = self.get_data_from_response(id3)
+        self.update_amount(id3, data)
+        
+
+# vk
+token = '08f8682608f8682608f86826850bec68c4008f808f868266ca567b35dc8f37858cb4172'
+version = 5.92
+first_id = 155632877
+
+
+popo = 1
+
+
 
 # Создаем экземпляр приложения
 app = SixDegreesApp()
@@ -81,5 +139,9 @@ app.add_connection("Борис", "Василий")
 app.add_connection("Василий", "Галина")
 app.add_connection("Галина", "Дмитрий")
 
+
 # З
-app.run()
+# app.run("Дмитрий", "Андрей")
+
+app.popopo(first_id)
+print(len(app.connections))
